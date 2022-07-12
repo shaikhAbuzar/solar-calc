a47530cb1d74df809103c5a9e8f32c6b4412b48b
Abuzar Shaikh <shaikhabuzar01@gmail.com>
2022-07-12 17:08:40 +0530

    Added the table support

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from flask import Flask, request, url_for, render_template
 from app.forms import UserInputForm
 from app.states.madhyapradesh import MadhyaPradesh
@@ -7,6 +8,53 @@ from app.states.gujarat import Gujarat
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'THISisNOTsoSECRET'
 app.config['DEBUG'] = True
+
+
+def create_table(principal, consumption):
+    rate = 8.48
+    annual_consumption = consumption * 12
+    maintenance_cost = 0
+    period = 25
+    net_savings = 0
+    break_even = False
+    data_dict = {
+        'Year': [],
+        'KWH': [],
+        'Rate': [],
+        'Savings': [],
+        'Maintenance': [],
+        'Net Savings': [],
+    }
+
+    for year in range(1, period + 1):
+        annual_output = annual_consumption * (0.90 if year <= 10 else 0.85)
+        savings = annual_output * rate
+        net_savings += savings - maintenance_cost
+
+        data_dict['Year'].append(year)
+        data_dict['KWH'].append(annual_output)
+        data_dict['Rate'].append(rate)
+        data_dict['Savings'].append(round(savings, 2))
+        data_dict['Maintenance'].append(maintenance_cost)
+        if not break_even and net_savings >= principal:
+            data_dict['Net Savings'].append(f'{str(round(net_savings, 2))} (Break Even)')
+            break_even = True
+        else:
+            data_dict['Net Savings'].append(str(round(net_savings, 2)))
+
+        rate += (rate * 0.03 if year <= 10 else 0)
+        if year == 3:
+            maintenance_cost = principal * 0.025
+        elif year > 3:
+            maintenance_cost += maintenance_cost * 0.05
+
+    rounded_df = pd.DataFrame(data_dict).round(2)
+    # print(rounded_df.to_html())
+    return rounded_df.to_html(
+        index=False,
+        classes='table table-bordered table-hover table-responsive',
+        justify='justify-all'
+    )
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -45,7 +93,9 @@ def home():
             # print(f'Profit Years: {profit_years}')
             # print(f'Connection Type: {ctype}')
 
-            return render_template('results.html', results=results)
+            data_table = create_table(results['cost'], consumption)
+
+            return render_template('results.html', results=results, data_table=data_table)
 
         if state == 'MadhyaPradesh':
             consumption = int(input_form.consumption.data)
